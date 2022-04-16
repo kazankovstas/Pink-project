6e77b5d36a76485d21daa3b65a46e5f5b5ef61ec
Stanislav <kazankovstas.jb@gmail.com>
2022-04-16 18:47:10 +0300

    commit

--- scripts/form.js
+++ scripts/form.js
@@ -4,6 +4,7 @@ const form = document.querySelector(".form");
 const firstName = document.querySelector(".form_name");
 const secondName = document.querySelector(".form_second-name");
 const mail = form.querySelector("[name=mail]");
+const modal = document.querySelector(".modal");
 const formButtonError = document.querySelector(".modal__button");
 const formButtonSucces = document.querySelector(".modal__button_large");
 const succesModal = document.querySelector(".modal-succes");
@@ -27,4 +28,21 @@ formButtonError.addEventListener("click", function (evt) {
 formButtonSucces.addEventListener("click", function (evt) {
   evt.preventDefault();
   succesModal.classList.remove("modal_show");
+});
+
+// Закрытие попапа кликом вне его область
+window.addEventListener("mousedown", function (evt) {
+  if (evt.target.closest(".modal") === null) {
+    modal.classList.remove("modal_show");
+  }
+});
+
+// Закрытие попапа клавишей Esc
+window.addEventListener("keydown", function (evt) {
+  if (evt.key === "Escape") {
+    if (modal.classList.contains("modal_show")) {
+      evt.preventDefault();
+      modal.classList.remove("modal_show");
+    }
+  }
 });
